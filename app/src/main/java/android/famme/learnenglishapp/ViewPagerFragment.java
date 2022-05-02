039aba8284007519c1fb126bb9767d7abea10f3a
Andrew <andrespin@mail.ru>
2022-05-02 17:26:12 +0300

    0.0.3

--- app/src/main/java/android/famme/learnenglishapp/ViewPagerFragment.java
+++ app/src/main/java/android/famme/learnenglishapp/ViewPagerFragment.java
@@ -2,13 +2,27 @@ package android.famme.learnenglishapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class ViewPagerFragment extends Fragment {
+
+    public static ViewPager2 viewPager;
+    // The pager adapter, which provides the pages to the view pager widget.
+    private FragmentStateAdapter pagerAdapter;
+    // Arrey of strings FOR TABS TITLES
+    private String[] titles = new String[]{"Tab1", "Tab2", "Tab3"};
+    // tab titles
 
 
     @Override
@@ -17,4 +31,26 @@ public class ViewPagerFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_view_pager, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init();
+    }
+
+    private void init() {
+
+        viewPager = getActivity().findViewById(R.id.mypager);
+
+        pagerAdapter = new MyPagerAdapter(getActivity());
+
+        viewPager.setAdapter(pagerAdapter);
+//inflating tab layout
+        TabLayout tabLayout =( TabLayout) getActivity().findViewById(R.id.tab_layout);
+//displaying tabs
+        new TabLayoutMediator(tabLayout, viewPager,(tab, position) -> tab.setText(titles[position])).attach();
+    }
+
+
+
 }
