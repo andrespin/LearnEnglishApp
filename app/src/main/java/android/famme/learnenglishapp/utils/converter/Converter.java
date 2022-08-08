af4ba526f7792caf486b276b14dc64a819d214f4
Andrew <andrespin@mail.ru>
2022-08-08 20:52:02 +0300

    Скорректировано

--- app/src/main/java/android/famme/learnenglishapp/utils/converter/Converter.java
+++ app/src/main/java/android/famme/learnenglishapp/utils/converter/Converter.java
@@ -10,67 +10,16 @@ public class Converter implements IConverter {
     @Override
     public ArrayList<String> convertResultEntityToListOfResults(ResultEntity resultEntity) {
         ArrayList<String> list = new ArrayList<>();
-
-        try {
-            list.add(resultEntity.getPersonality());
-        } catch (NullPointerException e) {
-
-        }
-
-        try {
-            list.add(resultEntity.getShopping());
-        } catch (NullPointerException e) {
-
-        }
-
-        try {
-            list.add(resultEntity.getEducation());
-        } catch (NullPointerException e) {
-
-        }
-
-        try {
-            list.add(resultEntity.getFamily());
-        } catch (NullPointerException e) {
-
-        }
-
-        try {
-            list.add(resultEntity.getLifestyle());
-        } catch (NullPointerException e) {
-
-        }
-
-        try {
-            list.add(resultEntity.getClothes());
-        } catch (NullPointerException e) {
-
-        }
-
-        try {
-            list.add(resultEntity.getBooks());
-        } catch (NullPointerException e) {
-
-        }
-
-        try {
-            list.add(resultEntity.getCulture());
-        } catch (NullPointerException e) {
-
-        }
-
-        try {
-            list.add(resultEntity.getLife());
-        } catch (NullPointerException e) {
-
-        }
-
-        try {
-            list.add(resultEntity.getSummer());
-        } catch (NullPointerException e) {
-
-        }
-
+        list.add(resultEntity.getPersonality());
+        list.add(resultEntity.getShopping());
+        list.add(resultEntity.getEducation());
+        list.add(resultEntity.getFamily());
+        list.add(resultEntity.getLifestyle());
+        list.add(resultEntity.getClothes());
+        list.add(resultEntity.getBooks());
+        list.add(resultEntity.getCulture());
+        list.add(resultEntity.getLife());
+        list.add(resultEntity.getSummer());
         return list;
     }
 
